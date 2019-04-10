0b65427533a2f48557f856bd4c53a07cdde4439a
hepengfei <252281711@qq.com>
2019-04-11 00:42:55 +0800

    mailService study

---
+++ src/main/java/com/hpf/hello/service/MailService.java
@@ -0,0 +1,9 @@
+package com.hpf.hello.service;
+
+
+public interface MailService {
+
+    public void sendSimpleMail(String to,String subject,String content);
+
+    public void sendHtmlMail(String to, String subject, String content);
+}
